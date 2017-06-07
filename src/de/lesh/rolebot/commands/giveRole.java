de544c7ee1ac3befb352c89a102823f5353f9c18
Lesh <gamelesh@gmail.com>
2017-06-07 19:50:50 +0200

    Able to add the beginner role

--- src/de/lesh/rolebot/commands/giveRole.java
+++ src/de/lesh/rolebot/commands/giveRole.java
@@ -1,6 +1,7 @@
 package de.lesh.rolebot.commands;
 
 import de.lesh.rolebot.user.bannedList;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.User;
@@ -12,7 +13,7 @@ public class giveRole extends ListenerAdapter{
 	public void onMessageReceived(MessageReceivedEvent e){
 		Message msg = e.getMessage();
 		User user = e.getAuthor();
-		//User getUser = e.getGuild();
+		Member member = e.getMember();
 		
 		if(!msg.getRawContent().startsWith(".r") || bannedList.black.contains(e.getAuthor().getIdLong()) || e.getAuthor().isBot()) {
 			return;
@@ -23,10 +24,15 @@ public class giveRole extends ListenerAdapter{
 		String role = e.getMessage().getRawContent().split("\\s+", 2)[1];
 		
 		switch(role){
-			case "beginner" :  e.getGuild().getController().addRolesToMember(user, beginnerID); //.getRoleById(316125948544155649L).;
+			case "beginner" :  {
+				e.getGuild().getController().addRolesToMember(member, beginnerID).queue();
+				System.out.println("[UPGRADE] >> Added <Beginner> to " + member);
+				break;
+			}			//.getRoleById(316125948544155649L).;
 			case "medium" : 
+				break;
 			case "profi" : 
-				
+				break;
 			default : e.getChannel().sendMessage("Die angegebene Gruppe ist nicht vorhanden. \n Bitte wähle zwischen beginner , medium oder profi").queue();
 		}
 	}
