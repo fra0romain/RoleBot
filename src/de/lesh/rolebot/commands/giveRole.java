0bb91878e4678d4604322f6fff5635e8728734a3
Lesh <gamelesh@gmail.com>
2017-06-07 22:29:53 +0200

    PATCH 5

--- src/de/lesh/rolebot/commands/giveRole.java
+++ src/de/lesh/rolebot/commands/giveRole.java
@@ -1,6 +1,9 @@
 package de.lesh.rolebot.commands;
 
 import java.awt.Color;
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import de.lesh.rolebot.user.bannedList;
 import net.dv8tion.jda.core.EmbedBuilder;
@@ -9,11 +12,13 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.handle.GuildMemberAddHandler;
+import net.dv8tion.jda.core.handle.GuildMemberRemoveHandler;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class giveRole extends ListenerAdapter{
 
-	public void onMessageReceived(MessageReceivedEvent e){
+	public void onMessageReceived(MessageReceivedEvent e, GuildMemberAddHandler add, GuildMemberRemoveHandler rem){
 		Message msg = e.getMessage();
 		User user = e.getAuthor();
 		Member member = e.getMember();
@@ -23,25 +28,25 @@ public class giveRole extends ListenerAdapter{
 		}
 		
 		Role beginnerID = e.getJDA().getRoleById(316125948544155649L);
-		Role mediumID = e.getJDA().getRoleById(322077001014050817L);
-		Role profiID = e.getJDA().getRoleById(322076854955671563L);
+		Role mediumID = e.getJDA().getRoleById(316125835323113472L);
+		Role profiID = e.getJDA().getRoleById(316125663226626048L);
 		
 		String role = e.getMessage().getRawContent().split("\\s+", 2)[1];
 		EmbedBuilder eB = new EmbedBuilder();
 		
 		switch(role){
 			case "beginner" :  {
-				e.getGuild().getController().addRolesToMember(member, beginnerID).queue();
+				e.getGuild().getController().modifyMemberRoles(member, beginnerID, profiID).queue();
 				System.out.println("[UPGRADE] >> Added <Beginner> to " + member);
 				break;
 			}
 			case "medium" : {
-				e.getGuild().getController().addRolesToMember(member, mediumID).queue();
+				e.getGuild().getController().modifyMemberRoles(member, mediumID, profiID, beginnerID).queue();
 				System.out.println("[UPGRADE] >> Added <Medium> to " + member);
 				break;
 			}	
 			case "profi" : {
-				e.getGuild().getController().addRolesToMember(member, profiID).queue();
+				e.getGuild().getController().modifyMemberRoles(member, profiID, beginnerID, mediumID).queue();
 				System.out.println("[UPGRADE] >> Added <Profi> to " + member);
 				break;
 			}	
